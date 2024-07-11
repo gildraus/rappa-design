f0d4c2b3604e18dee98b4aed7aa80b16f91037f3
gildraus <milos.vujcic.997@gmail.com>
2024-07-11 13:50:14 +0200

    nav and footer structured

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,3 +1,20 @@
 export default function Footer() {
-  return <div>footer je ovde</div>;
+  return (
+    <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <div>fb</div>
+          <div>ig</div>
+          <div>tw</div>
+        </div>
+        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>Nikola Rapaić</div>
+          <div>{` • `}</div>
+          <div>{`© 2024`}</div>
+          <div>{` • `}</div>
+          <a href="/">Rappa Design Studio</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
